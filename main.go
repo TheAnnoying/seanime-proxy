bd9f97f2d2f94636b4b24a05743393dff13331ee
TheAnnoying <drork2018@gmail.com>
2026-07-24 03:09:05 +0300

    feat: if proxy_server_url is setup, run a proxy server instead of the regular backend

--- main.go
+++ main.go
@@ -2,7 +2,14 @@ package main
 
 import (
 	"embed"
-	"seanime/internal/server"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/viper"
 )
 
 //go:embed all:web
@@ -11,6 +18,57 @@ var WebFS embed.FS
 //go:embed internal/icon/logo.png
 var embeddedLogo []byte
 
+var proxyServerURL string
+
 func main() {
-	server.StartServer(WebFS, embeddedLogo)
+	runExternal := false
+
+	dataDir, err := os.UserConfigDir()
+	if err == nil {
+		dataDir = filepath.Join(dataDir, "Seanime")
+	}
+
+	if os.Getenv("SEANIME_DATA_DIR") != "" {
+		dataDir = os.Getenv("SEANIME_DATA_DIR")
+	}
+
+	if err == nil {
+		configPath := filepath.Join(dataDir, "config.toml")
+
+		viper.SetConfigName("config")
+		viper.SetConfigType("toml")
+		viper.SetConfigFile(configPath)
+
+		if err := viper.ReadInConfig(); err == nil {
+			proxyServerURL = viper.GetString("server.proxy_server_url")
+			if proxyServerURL != "" {
+				runExternal = true
+			}
+		}
+	}
+
+	if runExternal {
+		target, err := url.Parse(proxyServerURL)
+		if err != nil {
+			log.Fatalf("invalid target URL: %v", err)
+		}
+
+		proxy := &httputil.ReverseProxy{
+			Rewrite: func(r *httputil.ProxyRequest) {
+				r.SetURL(target)
+				r.Out.Host = r.In.Host
+			},
+		}
+
+		server := &http.Server{
+			Addr:    "127.0.0.1:43211",
+			Handler: proxy,
+		}
+
+		log.Printf("Proxy listening on http://127.0.0.1:43211 -> %s", target)
+		log.Fatal(server.ListenAndServe())
+	} else {
+		// server.StartServer(WebFS, embeddedLogo)
+		log.Println("Starting server with embedded web assets...")
+	}
 }
